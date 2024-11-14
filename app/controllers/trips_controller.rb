19a015f852b512bd849bf1474af4de38939a9b41
Alcy Torres <mxalcytorres@gmail.com>
2024-11-14 18:29:04 -0500

    Add trips show, create, update, destroy actions

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,9 +1,49 @@
 class TripsController < ApplicationController
 
+  # Index action to display all trips
   def index
     @trips = Trip.all
     render json: @trips
   end
 
+  # Show action to display a single trip
+  def show
+    @trip = Trip.find(params[:id])
+    render json: @trip
+  end
 
+  # Create action to add a new trip
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      render json: @trip, status: :created
+    else
+      render json: @trip.errors, status: :unprocessable_entity
+    end
+  end
+
+  # Update action to modify an existing trip
+  def update
+    @trip = Trip.find(params[:id])
+    if @trip.update(trip_params)
+      render json: @trip
+    else
+      render json: @trip.errors, status: :unprocessable_entity
+    end
+  end
+
+  # Destroy action to delete a trip
+  def destroy
+    @trip = Trip.find(params[:id])
+    @trip.destroy
+    head :no_content
+  end
+
+  # Strong parameters for trip
+  private
+
+  def trip_params
+    params.require(:trip).permit(:location, :latitude, :longitude, :country, :continent, :year, :highlights, :image_url)
+  end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Trip.create(
+  location: 'Doha',
+  latitude: 25.276987,
+  longitude: 51.520008,
+  country: 'Qatar',
+  continent: 'Asia',
+  year: 2022,
+  highlights: 'World Cup, Souq Waqif, Katara Cultural Village, The Pearl Qatar, Corniche',
+  image_url: 'https://imagedelivery.net/0LMYosKeo5o-LXOCjdKUuA/tourscanner.com/2022/06/things-to-do-in-Doha-Qatar.jpg/w=1200'
+)
